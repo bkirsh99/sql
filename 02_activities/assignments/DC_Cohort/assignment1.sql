8cfabc9aab299c9952155db904653eebabeb3a9b
bkirsh99 <bianca@Joses-MacBook-Pro.local>
2026-03-26 18:56:46 -0400

    First commit to assignment 1; changes to .sql file

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -7,8 +7,8 @@
 /* 1. Write a query that returns everything in the customer table. */
 --QUERY 1
 
-
-
+SELECT *
+FROM customer;
 
 --END QUERY
 
@@ -17,8 +17,10 @@
 sorted by customer_last_name, then customer_first_ name. */
 --QUERY 2
 
-
-
+SELECT *
+FROM customer
+ORDER BY customer_last_name, customer_first_name
+LIMIT 10;
 
 --END QUERY
 
@@ -37,8 +39,17 @@ Limit to 25 rows of output.
 */
 --QUERY 3
 
+SELECT *
+FROM customer_purchases
+WHERE product_id IN (4, 9)
+LIMIT 25;
 
-
+SELECT *,
+       quantity * cost_to_customer_per_qty AS price
+FROM customer_purchases
+WHERE customer_id >= 8
+  AND customer_id <= 10
+LIMIT 25;
 
 --END QUERY
 
@@ -50,7 +61,13 @@ columns and add a column called prod_qty_type_condensed that displays the word �
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
 --QUERY 4
 
-
+SELECT product_id,
+       product_name,
+       CASE
+           WHEN product_qty_type = 'unit' THEN 'unit'
+           ELSE 'bulk'
+       END AS prod_qty_type_condensed
+FROM product;
 
 
 --END QUERY
@@ -61,8 +78,17 @@ add a column to the previous query called pepper_flag that outputs a 1 if the pr
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
 --QUERY 5
 
-
-
+SELECT product_id,
+       product_name,
+       CASE
+           WHEN product_qty_type = 'unit' THEN 'unit'
+           ELSE 'bulk'
+       END AS prod_qty_type_condensed,
+       CASE
+           WHEN LOWER(product_name) LIKE '%pepper%' THEN 1
+           ELSE 0
+       END AS pepper_flag
+FROM product;
 
 --END QUERY
 
@@ -73,8 +99,12 @@ vendor_id field they both have in common, and sorts the result by market_date, t
 Limit to 24 rows of output. */
 --QUERY 6
 
-
-
+SELECT *
+FROM vendor
+INNER JOIN vendor_booth_assignments
+    ON vendor.vendor_id = vendor_booth_assignments.vendor_id
+ORDER BY market_date, vendor_name
+LIMIT 24;
 
 --END QUERY
 
@@ -87,8 +117,10 @@ Limit to 24 rows of output. */
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
 --QUERY 7
 
-
-
+SELECT vendor_id,
+       COUNT(*) AS counts
+FROM vendor_booth_assignments
+GROUP BY vendor_id;
 
 --END QUERY
 
@@ -100,8 +132,16 @@ of customers for them to give stickers to, sorted by last name, then first name.
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
 --QUERY 8
 
-
-
+SELECT c.customer_id,
+       c.customer_first_name,
+       c.customer_last_name,
+       SUM(cp.quantity * cp.cost_to_customer_per_qty) AS total_spent
+FROM customer c
+JOIN customer_purchases cp
+    ON c.customer_id = cp.customer_id
+GROUP BY c.customer_id, c.customer_first_name, c.customer_last_name
+HAVING total_spent > 2000
+ORDER BY c.customer_last_name, c.customer_first_name;
 
 --END QUERY
 
@@ -119,8 +159,14 @@ VALUES(col1,col2,col3,col4,col5)
 */
 --QUERY 9
 
+CREATE TABLE temp.new_vendor AS
+SELECT *
+FROM vendor;
 
-
+INSERT INTO temp.new_vendor
+    (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
+VALUES
+    (10, 'Thomass Superfood Store', 'Fresh Focused', 'Thomas', 'Rosenthal');
 
 --END QUERY
 
@@ -133,8 +179,11 @@ and year are!
 Limit to 25 rows of output. */
 --QUERY 10
 
-
-
+SELECT customer_id,
+       STRFTIME('%m', market_date) AS month,
+       STRFTIME('%Y', market_date) AS year
+FROM customer_purchases
+LIMIT 25;
 
 --END QUERY
 
@@ -147,7 +196,11 @@ but remember, STRFTIME returns a STRING for your WHERE statement...
 AND be sure you remove the LIMIT from the previous query before aggregating!! */
 --QUERY 11
 
-
-
+SELECT customer_id,
+       SUM(quantity * cost_to_customer_per_qty) AS total_spent
+FROM customer_purchases
+WHERE STRFTIME('%m', market_date) = '04'
+  AND STRFTIME('%Y', market_date) = '2022'
+GROUP BY customer_id;
 
 --END QUERY
